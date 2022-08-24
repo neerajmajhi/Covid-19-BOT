c477ac2b180febfa8a4e23434ac1f7c081a1b62a
neerajmajhi <neerajmanjhi08@gmail.com>
2022-08-24 20:25:45 +0530

    test

--- chat.py
+++ chat.py
@@ -11,7 +11,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
-
+#he
 FILE = "data.pth"
 data = torch.load(FILE)
 
